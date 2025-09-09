adca62b0d5a082b84a2028b1d5c2043a07a3c5ec
Jack Ransom <62351018+Jransom33@users.noreply.github.com>
2025-09-09 16:12:04 -0400

    changed the overlay rectangle to be blue

--- Sources/WeScan/Common/QuadrilateralView.swift
+++ Sources/WeScan/Common/QuadrilateralView.swift
@@ -46,7 +46,7 @@ final class QuadrilateralView: UIView {
     public var editable = false {
         didSet {
             cornerViews(hidden: !editable)
-            quadLayer.fillColor = editable ? UIColor(white: 0.0, alpha: 0.6).cgColor : UIColor(white: 1.0, alpha: 0.5).cgColor
+            quadLayer.fillColor = editable ? UIColor.blue.withAlphaComponent(0.6).cgColor : UIColor.blue.withAlphaComponent(0.5).cgColor
             guard let quad else {
                 return
             }
@@ -71,7 +71,7 @@ final class QuadrilateralView: UIView {
             guard oldValue != isHighlighted else {
                 return
             }
-            quadLayer.fillColor = isHighlighted ? UIColor.clear.cgColor : UIColor(white: 0.0, alpha: 0.6).cgColor
+            quadLayer.fillColor = isHighlighted ? UIColor.clear.cgColor : UIColor.blue.withAlphaComponent(0.6).cgColor
             if isHighlighted {
                 bringSubviewToFront(quadView)
             } else {
